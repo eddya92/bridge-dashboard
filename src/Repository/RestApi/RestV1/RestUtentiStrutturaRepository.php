ae09aac0c17af28ab21a66c7c53e9ea168b756cc
Federico Profeti <federico.profeti@2rstudio.it>
2022-05-17 17:04:13 +0200

    Rimosso filtro periodo da la mia struttura

--- src/Repository/RestApi/RestV1/RestUtentiStrutturaRepository.php
+++ src/Repository/RestApi/RestV1/RestUtentiStrutturaRepository.php
@@ -42,7 +42,8 @@ final class RestUtentiStrutturaRepository implements UtentiStrutturaRepository, 
 			$response = $this->restApiConnection()
 				->withAuthentication($this->authenticationToken())
 				->client()
-				->request('GET', '/db-v1/utenti/struttura' . '?gruppo_di=' . $filtroGruppoDi . '&nominativo=' . $filtroNominativo . '&email=' . $filtroEmail . '&cellulare=' . $filtroCellulare . '&periodo=' . $filtroPeriodo . '&solo_diretti=' . $filtroDiretti . '&ordinamento=' . $filtroColonnaOrdinamento . '&direzione=' . $filtroDirezioneOrdinamento . '&items=' . $items . '&tipologia_utenza=' . $tipologiaUtenza . '&pag=' . $pag);
+				//->request('GET', '/db-v1/utenti/struttura' . '?gruppo_di=' . $filtroGruppoDi . '&nominativo=' . $filtroNominativo . '&email=' . $filtroEmail . '&cellulare=' . $filtroCellulare . '&periodo=' . $filtroPeriodo . '&solo_diretti=' . $filtroDiretti . '&ordinamento=' . $filtroColonnaOrdinamento . '&direzione=' . $filtroDirezioneOrdinamento . '&items=' . $items . '&tipologia_utenza=' . $tipologiaUtenza . '&pag=' . $pag);
+				->request('GET', '/db-v1/utenti/struttura' . '?gruppo_di=' . $filtroGruppoDi . '&nominativo=' . $filtroNominativo . '&email=' . $filtroEmail . '&cellulare=' . $filtroCellulare . '&solo_diretti=' . $filtroDiretti . '&ordinamento=' . $filtroColonnaOrdinamento . '&direzione=' . $filtroDirezioneOrdinamento . '&items=' . $items . '&tipologia_utenza=' . $tipologiaUtenza . '&pag=' . $pag);
 			$item->expiresAfter($this->ttlForUtentiStruttura);
 			$item->tag($this->authenticatedCacheTag(self::TAG_UTENTI_STRUTTURA));
 
