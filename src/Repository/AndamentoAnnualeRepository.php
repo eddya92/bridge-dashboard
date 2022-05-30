0fc671f3ed4dffe3fc5c561cfc36cc36d80da426
eddy <eddya92@hotmail.it>
2022-05-30 18:22:11 +0200

    [WIP] Pagina carriera personale

--- src/Repository/AndamentoAnnualeRepository.php
+++ src/Repository/AndamentoAnnualeRepository.php
@@ -8,5 +8,14 @@ use Generator;
 interface AndamentoAnnualeRepository{
 	public const TAG_ANDAMENTO_ANNUALE = 'andamentoAnnuale';
 
-	public function getCarrieraAnnuale(string $locale, string $filtroColonnaOrdinamento, string $filtroDirezioneOrdinamento) : ?Generator;
+	/**
+	 * Andamento mese per mese di un utente(Chiamata che va a riempire il datatable nella pagina della carriera)
+	 *
+	 * @param string $locale
+	 * @param string $filtroColonnaOrdinamento
+	 * @param string $filtroDirezioneOrdinamento
+	 *
+	 * @return Generator
+	 */
+	public function getCarrieraAnnuale(string $locale, string $filtroColonnaOrdinamento, string $filtroDirezioneOrdinamento, string $pag, string $items) : Generator;
 }
